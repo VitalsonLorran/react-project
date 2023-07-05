c763e947bf39ee5c8bc0d5121fee3459cbb025f3
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-07-05 11:27:32 -0300

    Renderizando algumas infos
    Consegui colocar algumas informacoes na tela.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import { students } from "@/students"
+
 const Page = () => {
+  const studentsList = students
+
   return (
-    <div className=" flex bg-sky-500 w-screen h-screen justify-center items-center">
-      
-      <p>Ola</p>
-      
+    <div className="flex flex-col bg-black w-screen h-screen items-align items-center">
+      <div className="flex justify-evenly h-10 p-5 w-4/5 rounded-t bg-slate-800 items-center">
+        <div>Name</div>
+        <div>Status</div>
+        <div>Grade 1</div>
+        <div>Grade 2</div>
+        <div>Final Grade</div>
+      </div>
+      <div>
+        <ul>
+           {studentsList.length > 0 &&
+            studentsList.map(i =>
+              <div className="flex p-2 justify-evenly w-screen bg-slate-500"> 
+              <li>{i.name}</li>
+              <li>{i.grade1}</li>
+              <li>{i.grade2}</li>
+              </div>
+              )
+           }
+        </ul>
+      </div>
     </div>
   )
 }
