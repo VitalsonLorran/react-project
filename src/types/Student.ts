aaf405cb07d8bc50477bb52d6be2124cbe58f0b7
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-07-04 21:24:22 -0300

    Colocar um item no meio da tela
    tava com problema para fazer funcionar o "justify-center" e o  "items-center" (colocar um item no meio da tela) porem percebi que precisava aumentar o tamanho da minha div para w-screen e h-screen

---
+++ src/types/Student.ts
@@ -0,0 +1,12 @@
+export type Student = 
+    
+    {
+        id: Number,
+        active: Boolean,
+        name: String,
+        email: String,
+        avatar: String,
+        grade1: Number,
+        grade2: Number
+    }
+    
