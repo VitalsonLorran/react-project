aadf924aecf416185c150e61216aaa5121c36fad
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-07-06 22:35:23 -0300

    Arrumando um pouco a lista de nomes.
    Ainda preciso botar foto, email e etc.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { students } from "@/students"
-import { useState } from "react"
 import { InputText } from "@/components/InputText"
 
 const Page = () => {
@@ -10,20 +9,22 @@ const Page = () => {
   return (
     <div className="flex flex-col bg-black w-screen h-screen items-align items-center">
       <div className="flex justify-evenly h-10 p-5 w-4/5 rounded-t bg-slate-800 items-center">
-        <div>Name</div>
+        <div className="w-2/5">Name</div>
         <div>Status</div>
         <div>Grade 1</div>
         <div>Grade 2</div>
         <div>Final Grade</div>
       </div>
-      <div>
-        <ul>
+      <div className="w-full">
+        <ul className="flex flex-col justify-center items-center">
            {studentsList.length > 0 &&
             studentsList.map(i =>
-              <div className="flex p-2 justify-evenly w-screen bg-slate-500"> 
-              <li>{i.name}</li>
-              <li>{i.grade1}</li>
-              <li>{i.grade2}</li>
+              <div className="flex justify-evenly p-2 w-4/5 bg-slate-500"> 
+                <li className="w-2/5">{i.name}</li>
+                <li>Status</li>
+                <li>{i.grade1}</li>
+                <li>{i.grade2}</li>
+                <li>Final grade</li>
               </div>
               )
            }
